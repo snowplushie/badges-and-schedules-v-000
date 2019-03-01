2f2e4593246c70df684138e67850d0367758f712
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 04:28:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here
 
-speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   puts "Hello, my name is #{name}."
