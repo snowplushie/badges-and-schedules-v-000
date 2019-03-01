58a73d7b243303ad40afb45ae4291e9896ca7c0a
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 04:31:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 
 speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(Arel)
-  puts "Hello, my name is #{Arel}."
+def badge_maker(speaker)
+  puts "Hello, my name is #{speaker}."
 end
 
