fa942bfaf0d7efc6cb6ed3a788d6ad9713019b0d
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 04:45:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ def batch_badge_creator(attendees)
 end
 end
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+def assign_rooms(attendee)
+  attendees.each_with_index.collect do |attendee, index|
   "Hello, #{attendee}! You'll be assigned to room #{index}!"
 end
 end
