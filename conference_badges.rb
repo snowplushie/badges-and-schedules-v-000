85dc7e2a458e7805ec7fa02a47e2b86a91182ff9
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 04:53:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,7 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
-    assign_rooms(attendees).each do 
+    assign_rooms(attendees).each do |assignment|
+      puts assignment
   end
 end
